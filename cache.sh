10886805c13a0e49fd3d5387c21a60fad3b284fc
Grzegorz Patynek <gpatynek@gmail.com>
2016-01-25 00:45:59 +0100

    cache and publish

--- cache.sh
+++ cache.sh
@@ -1,6 +1,7 @@
 #cache - download a webpage to disc, cache it in gzip and add to website
 #header footer file should be present also git in filesystem and wget
 #usage cache.sh WEBSITE_URL TITLE DESCRIPTION
+cd ~/skrypty/cache/
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 mkdir cache/$NEW_UUID
 cd cache
@@ -8,18 +9,17 @@ wget -P $NEW_UUID -p -k $1
 tar -cvzf $NEW_UUID.tar.gz $NEW_UUID
 cp $NEW_UUID.tar.gz ../stronaDomowa/strony/
 
-link = '\r\n<li><a href="'$1'">'$2'</a><a href="strony/'$NEW_UUID.tar.gz'">cache</a>
-<blockquote>'$3'
+link='<li><a href="'$1'">'"$2"'</a> (<a href="strony/'$NEW_UUID.tar.gz'">cache</a>
+)<blockquote>'"$3"'
 </blockquote>
 </li>';
 
-cd ..
 echo $link >> links
 cat header > linki.html
 cat links >> linki.html
 cat footer >> linki.html
 
-cp linki.html -R stronaDomowa/
+cp linki.html -R ../stronaDomowa/
 
 git init
 git add .
